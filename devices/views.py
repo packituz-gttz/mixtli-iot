045aec81a920ad4b48a48b4ace22670c6f239756
Rafael <pacohm94@gmail.com>
2019-08-28 15:07:06 -0500

    Implemented GET/POST & update swagger to openapi 3

--- devices/views.py
+++ devices/views.py
@@ -1,5 +1,8 @@
 from flask import abort
 from models import Device
+from sqlalchemy.exc import OperationalError
+from models import db
+from connexion import NoContent
 
 
 def get_devices():
@@ -15,5 +18,35 @@ def get_devices():
     return devices_list
 
 
-def get_device():
-    pass
+def get_device(device_id):
+    """
+
+    :param device_id:
+    :return: Return device info
+    """
+    try:
+        device = Device.query.filter_by(id=device_id).first()
+        print("DEV", device)
+    except TimeoutError:
+        abort(408)
+    except Exception:
+        abort(500)
+
+    if device is None:
+        abort(404)
+    return device
+
+
+def post_device(device):
+    try:
+        new_device = Device(**device)
+        db.session.add(new_device)
+        db.session.commit()
+    except TimeoutError:
+        abort(400)
+    except OperationalError:
+        abort(503)
+    except Exception:
+        abort(500)
+
+    return NoContent, 204
