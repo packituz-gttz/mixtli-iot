c87408ce7a60aa32dfd6937bd726da96ea69175e
Rafael <pacohm94@gmail.com>
2019-08-28 20:05:55 -0500

    Updated models

--- devices/models.py
+++ devices/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from config import db
+import
 
 
 class BaseModel(db.Model):
@@ -22,6 +23,9 @@ class Device(BaseModel):
     name = db.Column(db.String(50))
     # type = db.Column() enum, string ?
     location = db.Column(db.String(300))
+    longitude = db.Column(db.Float)
+    latitude = db.Column(db.Float)
+    device_type= db.Column(db.Integer)
     device_status = db.Column(db.SmallInteger)
     hardware_version = db.Column(db.String(50))
     software_version = db.Column(db.String(50))
@@ -32,6 +36,7 @@ class Device(BaseModel):
     last_connection = db.Column(db.DateTime)
     meta_data = db.Column(db.JSON)
     product_id = db.Column(db.Integer, db.ForeignKey(Product.id))
+    blocked = db.Column(db.SmallInteger, server_default=0)
 
     def __repr__(self):
         return f'<Device {self.id}, name: {self.name}>'
