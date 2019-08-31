385e6798b2b972f43b9a81ea4e6407244201166d
Paco <pacohm94@gmail.com>
2019-08-31 16:50:26 -0500

    Added Post openapi specification

--- devices/config.py
+++ devices/config.py
@@ -2,6 +2,7 @@ import os
 import connexion
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -21,5 +22,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # Create the SqlAlchemy db instance
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+migrate = Migrate(app, db)
 
 
